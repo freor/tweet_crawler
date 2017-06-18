2abb69ac6c08f6ec21f4a80527788161c5e81dce
freor <befreor@gmail.com>
2017-06-19 03:45:44 +0900

    date check

---
+++ p.py
@@ -0,0 +1,6 @@
+#a = '\uc624'
+#a = '\uc624\ud6c424234'
+a = "\uc624\ud6c4 4:54 - 2017\ub144 5\uc6d4 27\uc77c"
+print(a.replace("오후","PN"))
+print(a)
+
--- q.py
+++ q.py
@@ -1,13 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from bs4 import BeautifulSoup
-from datetime import date
+from datetime import datetime
 from datetime import timedelta
 import urllib.request as ur
 import json
 
+
+
 url1 = "https://twitter.com/i/search/timeline?f=tweets&vertical=default&q=%EB%AC%B8%EC%9E%AC%EC%9D%B8%20since%3A"
-date1 = "2017-05-27"
+date1 = "2017-05-09"
 url2 = "%20until%3A"
-date2 = "2017-05-28"
+date2 = "2017-05-10"
 url3 = "&src=typd&include_available_features=1&include_entities=1&lang=ko&max_position=TWEET-"
 num1 = "868616452256837634"
 #-
@@ -18,11 +23,13 @@ tweet = {}
 
 headers = {}
 headers['User-Agent'] = "chrome/5.0"
+count = 0
 
-n = 100
+#n = 100
+init_date = datetime(2017, 5, 9)
 
-while(n):
-    n = n - 1
+while(1):
+    #n = n - 1
     url = url1 + date1 + url2 + date2 + url3 + num1 + '-' + num2 + url4
     
     req = ur.Request(url, headers = headers)
@@ -30,12 +37,48 @@ while(n):
     soup = BeautifulSoup(fp, "lxml")
     p = soup.find("p")
     q = p.text
-    count = q.count("js-tweet-text-container") # count # of tweets
+    count += q.count("js-tweet-text-container") # count # of tweets
     num1 = q[23:41]
     num2 = q[42:60]
     print("num1: "+num1)
     print("num2: "+num2)
-    print(count)
+    print("count: "+str(count))
+    print("index")
+    #print(q.find('class=\"tweet-timestamp js-permalink js-nav js-tooltip\" title='))
+    #  js-permalink js-nav js-tooltip\" title=\" : LEN(40)
+    # js-permalink js-nav js-tooltip\" title=\"\uc624\ud6c4 4:48 - 2017\ub144 6\uc6d4 13\uc77c\"  dat : LEN(67)
+    '''
+    idx = q.find('tweet-timestamp')
+    print(idx) # 2724
+    '''
+    idx = q.rfind('tweet-timestamp') # find LAST occurence of "tweet-timestamp"
+    print(idx)
+    cand = q[idx+57:idx+115]
+    i = cand.find('"')
+    time = cand[0:i-1]
+    # using json to encode and decode "utf-8"
+    tmp = '{"a":"'+time+'"}'
+    tmp = json.loads(tmp)
+    time = tmp["a"]
+
+    #print(time.encode('utf-8').decode('utf-8'))
+    time = time.replace("오후", "PM")
+    time = time.replace("오전", "AM")
+    new_time = datetime.strptime(time, "%p %I:%M - %Y년 %m월 %d일")
+    new_time = datetime(new_time.year, new_time.month, new_time.day)
+    if(init_date != new_time):
+        break
+
+    print(new_time)
+
+    '''
+    # DEBUG
+    print("IDX:"+str(cand.find('"')))
+    print(cand[0:i])
+    print(q.find('tweet-timestamp'))
+    print(q.find("min"))
+    '''
+
 
 '''
 url = url1 + date1 + url2 + date2 + url3 + num1 + '-' + num2 + url4
